4333a9924717b351f04f2982959f06f2a6ce62a3
Adarsh Kaushal <Adarshkaushal14@gmail.com>
2025-02-25 18:32:22 +0530

    changed form color

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -69,34 +69,34 @@
   </div>
     <!-- Fixed Form -->
     <div class="absolute hidden  top-2/3 lg:bottom-auto lg:top-1/2 lg:right-6 lg:-translate-y-1/2 w-full lg:h-[80%] h-[32rem] lg:flex items-start max-w-lg">
-      <div class="bg-white rounded-lg p-6 shadow-xl lg:h-[70%] ">
-        <h2 class="text-[32px] text-center font-bold mb-2 text-[#212B27]">Get in touch</h2>
-        <p class="text-[#32403B] text-[14px] mb-6">We're committed to providing the best support for our traders!</p>
+      <div class="bg-[#14171a] rounded-lg p-6 shadow-xl lg:h-[70%] ">
+        <h2 class="text-[32px] text-center font-bold mb-2 text-[#ffffff]">Get in touch</h2>
+        <p class=" text-[14px] mb-6 text-white">We're committed to providing the best support for our traders!</p>
 
         <form @submit.prevent="handleSubmit" class="">
           <div>
-            <label class="block text-sm font-medium text-[#6B7888]">Full Name</label>
+            <label class="block text-sm font-medium text-[#ffffff]">Full Name</label>
             <input type="text" v-model="formData.name"
-              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              class="mt-1 block w-full px-3 py-2 border border-white border-opacity-10 bg-[#ffffff11] text-white rounded-md shadow-sm "
               required>
           </div>
 
           <div class="mt-4">
-            <label class="block text-sm font-medium text-[#6B7888]">Email</label>
+            <label class="block text-sm font-medium text-[#ffffff]">Email</label>
             <input type="email" v-model="formData.email"
-              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              class="mt-1 block w-full px-3 py-2 border border-white border-opacity-10 bg-[#ffffff11] text-white rounded-md shadow-sm "
               required>
           </div>
 
           <div class="mt-4">
-            <label class="block text-sm font-medium text-[#6B7888]">Number</label>
+            <label class="block text-sm font-medium text-[#ffffff]">Number</label>
             <input type="tel" v-model="formData.number"
-              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              class="mt-1 block w-full px-3 py-2 border border-white border-opacity-10 bg-[#ffffff11] text-white rounded-md shadow-sm "
               required>
           </div>
 
           <button type="submit"
-            class="w-full bg-[#191D23] mt-4 text-white py-2 px-4 rounded-md hover:bg-gray-800 transition-colors">
+            class="w-full bg-[#4984c4] mt-4 text-white py-2 px-4 rounded-md  transition-colors">
             Submit
           </button>
         </form>
